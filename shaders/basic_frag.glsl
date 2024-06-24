e33c838fee0208a48ea6134979827b6d7115847b
Marcus Gigandet <78104059+SlyFryFrog@users.noreply.github.com>
2024-06-23 21:07:28 -0400

    Add clang-format and .gitignore

--- shaders/basic_frag.glsl
+++ shaders/basic_frag.glsl
@@ -10,14 +10,14 @@ in vec4 foliageColor;
 
 void main()
 {
-    vec4 outputColorData = texture(gtexture, texCoord);
-    vec3 outputColor = outputColorData.rgb * foliageColor.rgb;
-    float transparency = outputColorData.a;
+	vec4 outputColorData = texture(gtexture, texCoord);
+	vec3 outputColor = outputColorData.rgb * foliageColor.rgb;
+	float transparency = outputColorData.a;
 
-    if (transparency < 0.1)
-    {
-        discard;
-    }
+	if (transparency < 0.1)
+	{
+		discard;
+	}
 
-    outColor0 = vec4(outputColor, transparency);
+	outColor0 = vec4(outputColor, transparency);
 }
--- shaders/gbuffers_terrain.vsh
+++ shaders/gbuffers_terrain.vsh
@@ -3,6 +3,7 @@
 in vec3 vaPosition;
 in vec2 vaUV0;
 in vec4 vaColor;
+in ivec2 vaUV2;
 
 uniform mat4 modelViewMatrix;
 uniform mat4 projectionMatrix;
@@ -10,10 +11,12 @@ uniform vec3 chunkOffset;
 
 out vec2 texCoord;  // Passes to fsh
 out vec4 foliageColor;
+out ivec2 lightMapCoords;
 
 void main()
 {
     texCoord = vaUV0;
     foliageColor = vaColor;
+    lightMapCoords = vaUV2;
     gl_Position = projectionMatrix * modelViewMatrix * vec4(vaPosition + chunkOffset, 1); // Perspective
 }
